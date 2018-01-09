c5ade7e5a64bbf25a2495f4949022be9aea8f4b1
Yann Le Corre <yann.lecorre@uni.lu>
2018-01-09 15:41:24 +0100

    YLC: Added missing license headers

--- python/gen_trace.py
+++ python/gen_trace.py
@@ -1,4 +1,31 @@
 #! /usr/bin/env python
+################################################################################
+# 
+#  University of Luxembourg
+#  Laboratory of Algorithmics, Cryptology and Security (LACS)
+# 
+#  arm_v7m_leakage simulator
+# 
+#  Copyright (C) 2017 University of Luxembourg
+# 
+#  Written in 2017 by Yann Le Corre <yann.lecorre@uni.lu> and
+#  Daniel Dinu <daniel.dinu@uni.lu>
+# 
+#  This simulator is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  It is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, see <http://www.gnu.org/licenses/>.
+# 
+################################################################################
+
 
 regs_keys = ("r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r9", "r10", "r11", "r12", "r13", "r13", "rA", "rB")
 
